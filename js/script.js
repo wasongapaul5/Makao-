45ee9cf30f6642346118f55bba01a56e45106f49
owinolawrence <owinolawrence21@gmail.com>
2019-10-09 11:15:55 +0300

    Create slidedown effects on class para and image

--- js/script.js
+++ js/script.js
@@ -9,20 +9,32 @@
 // }
 
 $(document).ready(function () {
-    $("#image1").click(function () {
-      $(".p").toggle();
+    $("#image").click(function () {
+     $(".para").slideToggle("slow");
+    }); $("#image0").click(function () {
+        $(".para0").slideToggle("slow");
+    }); $("#image1").click(function () {
+        $(".para1").slideToggle("slow");
+    }); $("#image2").click(function () {
+        $(".para2").slideToggle("slow");
+    }); $("#image3").click(function () {
+        $(".para3").slideToggle("slow");
+    }); $("#image4").click(function () {
+        $(".para4").slideToggle("slow");
+    }); $("#image5").click(function () {
+        $(".para5").slideToggle("slow");
+    }); $("#image6").click(function () {
+        $(".para6").slideToggle("slow");
+    }); $("#image7").click(function () {
+        $(".para7").slideToggle("slow");
+    }); $("#image8").click(function () {
+        $(".para8").slideToggle("slow");
+    }); $("#image9").click(function () {
+        $(".para9").slideToggle("slow");
+    }); 
+    $("#image10").click(function () {
+        $(".para10").slideToggle("slow");
     });
-    $(".image2").click(function () {
-      $(".p").toggle();
-    });
-    $(".image3").click(function () {
-      $(".p").toggle();
-    });
-    $(".image").hover(function () {
-      $(this).css({ opacity: "0.7", "transition":"2s" });
-    },
-      function () {
-        $(this).css({ opacity: "0.7" ,"transition":"2s" });
-      });
-  });
-  
+});
+
+
